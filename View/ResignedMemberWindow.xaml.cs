887797e739fcf3235487378a6a78596beb57d9de
thankPark <psch2506@gmail.com>
2025-10-30 09:15:20 +0900

    [박정희] 탈퇴회원조회시 사진 안나오는 오류 수정, 회원 탈퇴시 MainWindow UI에 바로 반영, 반납 화면 가운데 정렬

--- View/ResignedMemberWindow.xaml.cs
+++ View/ResignedMemberWindow.xaml.cs
@@ -21,6 +21,12 @@ namespace library_management_system.View
             InitializeComponent();
         }
 
+        // 공개 갱신 메서드: 외부에서 호출하여 목록 재로딩 가능
+        public async Task RefreshAsync()
+        {
+            await LoadDataAsync();
+        }
+
         private void ResignedMember_close(object sender, RoutedEventArgs e)
         {
             if (System.Windows.Application.Current.MainWindow is MainWindow main)

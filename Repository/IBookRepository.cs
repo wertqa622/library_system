8a1a6c91ef9a994c2061f75f8c1d7410314b6024
thankPark <psch2506@gmail.com>
2025-10-30 10:03:16 +0900

    [박정희] 도서삭제오류 수정

--- Repository/IBookRepository.cs
+++ Repository/IBookRepository.cs
@@ -6,9 +6,15 @@ namespace library_management_system.Repository
 {
     public interface IBookRepository
     {
-        Task<IEnumerable<Book>> GetAllBooksAsync();        
+        Task<IEnumerable<Book>> GetAllBooksAsync();
+
         Task<Book> AddBookAsync(Book book);
+
         Task<Book> UpdateBookAsync(Book book);
+
         Task<bool> DeleteBookAsync(string isbn);
+
+        // 부모(BOOK) 삭제 시 관련 LOAN(대출 이력)도 함께 삭제하는 메서드 추가
+        Task<bool> DeleteBookAndLoansAsync(string isbn);
     }
 }
